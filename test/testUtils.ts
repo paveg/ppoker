b472e6806791275eac73aa75ff3b2745713b421e
Ryota Ikezawa <pavegy@gmail.com>
2021-05-28 21:13:00 +0900

    :sparkles: Update

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -14,7 +14,7 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui: any, options = {}) =>
+const customRender = (ui, options = {}) =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
